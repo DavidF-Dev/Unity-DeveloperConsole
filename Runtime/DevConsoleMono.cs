6913cbafb5bd75f4d5467668e988bd3b4da70e70
DavidF-Dev <account@davidfdev.com>
2021-10-30 16:47:20 +1100

    Updated version.

--- Runtime/DevConsoleMono.cs
+++ Runtime/DevConsoleMono.cs
@@ -110,7 +110,7 @@ namespace DavidFDev.DevConsole
 
         #endregion
 
-        private static readonly Version _version = new Version(1, 0, 2);
+        private static readonly Version _version = new Version(1, 0, 3);
         private static readonly string[] _permanentCommands =
         {
             "devconsole", "commands", "help", "print", "clear", "reset", "bind", "unbind", "bindings"
